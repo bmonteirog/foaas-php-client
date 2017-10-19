66972a4da1442f0e9fbd619d1c5fb9c63dfbf113
Saad Nasir <sa>
2017-10-19 20:21:08 +0500

    Guzzle implementation

--- src/Bmonteirog/FOAAS/Client.php
+++ src/Bmonteirog/FOAAS/Client.php
@@ -37,17 +37,13 @@ class Client implements ClientInterface
         if(!is_null($arg3))
             $url .= '/'.$arg3;
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_USERAGENT, $this->clientInfo);
-        
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [
-          'Accept: ' . $this->accept
-        ]);
-        
-        $data = curl_exec($curl);
-        curl_close($curl);
+        $client = new \GuzzleHttp\Client();
+        $headers = [
+            'User-Agent' => $this->clientInfo,
+            'Accept'     => $this->accept
+        ];
+        $request = $client->request('GET', $url, $headers);
+        $data = $request->getBody();
         return $data;
     }
     
